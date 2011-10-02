d04ebe8937529caf407e0d6218fcdbc707884219
Pece Milosev <pmilosev@gmail.com>
2011-10-02 17:03:41 +0200

    General cl_proposition cleanup.
    Inheriting from cl_object.

---
+++ src/cl_object_rep.h
@@ -0,0 +1,33 @@
+/*
+ *   Copyright (C) 2011  Pece Milosev
+ *
+ *   This file is part of 'clumsy'.
+ *   'clumsy' is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   'clumsy' is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CL_OBJECT_REP_H
+#define CL_OBJECT_REP_H
+
+#include "cl_object.h"
+
+struct cl_object_info_s {
+	size_t _ref;
+	cl_object_destructor _dest;
+};
+
+struct cl_object_s {
+	cl_object_info _obj_info;
+};
+
+#endif /* CL_OBJECT_REP_H */
---
+++ src/cl_proposition_rep.h
@@ -0,0 +1,31 @@
+/*
+ *   Copyright (C) 2011  Pece Milosev
+ *
+ *   This file is part of 'clumsy'.
+ *   'clumsy' is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   'clumsy' is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CL_PROPOSITION_REP_H
+#define CL_PROPOSITION_REP_H
+
+#include "cl_proposition.h"
+#include "cl_object_rep.h"
+
+struct cl_proposition_s {
+	cl_object_info _obj_info;
+	size_t _depth;
+	cl_proposition_context _context;
+};
+
+#endif /* CL_PROPOSITION_REP_H */
